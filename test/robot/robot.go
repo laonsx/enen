90539da2c1dc10e1256ace88b298dcedfb47ecac
DESKTOP-OBN500O\zhang <zhang13652103006@163.com>
2018-08-22 18:19:57 +0800

    update test

--- test/robot/robot.go
+++ test/robot/robot.go
@@ -1,4 +1,4 @@
-package main
+package robot
 
 import (
 	"crypto/hmac"
@@ -6,74 +6,153 @@ import (
 	"encoding/base64"
 	"encoding/binary"
 	"encoding/json"
-	"flag"
 	"fmt"
-	"log"
 	"math/big"
-	"mynet/lib/crypt"
-	"mynet/lib/gofunc"
-	"mynet/lib/timer"
-	"mynet/pb"
-	"os"
-	"os/signal"
-	"syscall"
+	"strconv"
+	"strings"
 	"time"
+
+	"enen/common/pb"
 
 	"github.com/golang/protobuf/proto"
 	"github.com/gorilla/websocket"
+	"github.com/laonsx/gamelib/crypt"
+	"github.com/laonsx/gamelib/timer"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
 )
 
-var quit = make(chan os.Signal, 1)
+func Test() {
 
-func main() {
+	test := viper.GetString("test.robot")
+	cmds := strings.Split(test, "_")
+	if len(cmds) != 2 {
 
-	id := flag.String("id", "robot", "-id")
-	num := flag.Int("num", 0, "-num")
-	flag.Parse()
+		panic("test.robot cmd = " + test)
+	}
 
-	if *num == 0 {
-		fmt.Println("./robot -id=robot -num=100")
-		return
+	id := cmds[0]
+	count, err := strconv.Atoi(cmds[1])
+	if err != nil {
+
+		panic("test.robot cmd err = " + err.Error())
 	}
 
 	defer func() {
-		log.Println("done")
+
+		logrus.Println("done")
 	}()
 
-	timer.AfterFunc(1*time.Second, *num, func(n int) {
-		token := fmt.Sprintf("%s%d", *id, n)
+	timer.AfterFunc(100*time.Millisecond, count, func(n int) {
+
+		token := fmt.Sprintf("%s_%d", id, n)
 		start(token)
+
+		logrus.Info(token + " login")
+	})
+}
+
+func start(token string) {
+
+	addr, secret, uid := login(token)
+
+	logrus.Debugf("connecting to gate", uid)
+
+	d := websocket.Dialer{}
+	conn, _, err := d.Dial(fmt.Sprintf("ws://%s/ws", addr), nil)
+	if err != nil {
+
+		logrus.Fatal("dial: ", err)
+
+		return
+	}
+
+	req := new(pb.GateRequest)
+	req.Secret = secret
+	req.Uid = uid
+
+	b, _ := proto.Marshal(req)
+	_, code, _, err := request(conn, 3, b)
+	if err != nil || code != 0 {
+
+		return
+	}
+
+	logrus.Debugf("gate auth finished", uid)
+
+	u := newUser(uid, conn)
+	u.pnum(1001)
+
+	//挂机开始
+	timer.AfterFunc(10*time.Second, 0, func(n int) {
+
+		u.pnum(1)
 	})
 
-	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
-	<-quit
+	timer.AfterFunc(2*time.Second, 0, func(n int) {
+
+		req := &pb.HelloRequest{}
+		req.ReqMsg = "哈哈。。hello..服务端。。nice to meet you"
+		data, err := proto.Marshal(req)
+		if err != nil {
+
+			logrus.Errorf("marshal 1001 err = %v", err)
+			return
+		}
+		u.send(pack(1001, data))
+	})
+
+}
+
+func (u *user) printInfo(pnum uint16, body []byte) {
+
+	switch pnum {
+
+	case 1002:
+
+		resp := pb.HelloResponse{}
+		err := proto.Unmarshal(body, &resp)
+		if err != nil {
+
+			logrus.Errorf("unmarshal 1002 err = %v", err)
+		}
+
+		logrus.Debugf("1002 return =>", resp.RespMsg)
+	}
 }
 
 func login(token string) (string, string, uint64) {
-	log.Println("connecting to login")
+
+	logrus.Debugf("connecting to center")
+
 	d := websocket.Dialer{}
-	c, _, err := d.Dial("ws://119.29.6.220:10002/ws", nil)
+	c, _, err := d.Dial("ws://10.1.16.69:8002/ws", nil)
 	if err != nil {
-		log.Fatal("dial: ", err)
+
+		logrus.Fatal("dial: ", err)
 	}
 
 	defer c.Close()
 
 	_, challenge, err := c.ReadMessage()
 	if err != nil {
-		log.Fatal(err)
+
+		logrus.Fatal(err)
 	}
 
 	privatekey := crypt.Randomkey()
 	clientkey := crypt.DHExchange(privatekey)
+
 	err = c.WriteMessage(websocket.TextMessage, []byte(clientkey.String()))
 	if err != nil {
-		log.Fatal(err)
+
+		logrus.Fatal(err)
 	}
 
 	_, serverkey, err := c.ReadMessage()
 	if err != nil {
-		log.Fatal(err)
+
+		logrus.Fatal(err)
 	}
 
 	exchangekey := new(big.Int)
@@ -86,73 +165,36 @@ func login(token string) (string, string, uint64) {
 	clientmac := base64.StdEncoding.EncodeToString(out)
 	err = c.WriteMessage(websocket.TextMessage, []byte(clientmac))
 	if err != nil {
-		log.Fatal(err)
+
+		logrus.Fatal(err)
 	}
+
 	_, ok, err := c.ReadMessage()
 	if string(ok) != "ok" {
-		log.Fatal("climac err")
+
+		logrus.Fatal("climac err")
 	}
 
 	c.WriteMessage(websocket.TextMessage, []byte(token+":test"))
 
 	_, info, err := c.ReadMessage()
 	if err != nil {
-		log.Fatal(err)
+
+		logrus.Fatal(err)
 	}
 
 	v := make(map[string]interface{})
 	err = json.Unmarshal(info, &v)
 	if err != nil {
-		log.Println(err.Error())
+
+		logrus.Println(err.Error())
 	}
-	log.Println("login finished", v)
+
+	logrus.Debugf("center finished", v)
 
 	uid := v["uid"].(float64)
+
 	return v["addr"].(string), secret.String(), uint64(uid)
-}
-
-func start(token string) {
-	addr, secret, uid := login(token)
-
-	log.Println("connecting to gate", uid)
-
-	d := websocket.Dialer{}
-	conn, _, err := d.Dial(fmt.Sprintf("ws://%s/ws", addr), nil)
-	if err != nil {
-		log.Fatal("dial: ", err)
-		return
-	}
-
-	req := new(pb.AuthRequest)
-	req.Secret = proto.String(secret)
-	req.Uid = proto.Uint64(uid)
-	b, _ := proto.Marshal(req)
-	_, code, _, err := request(conn, 3, b)
-	if err != nil || code != 0 {
-		return
-	}
-
-	log.Println("auth finished", uid)
-
-	u := newUser(uid, conn)
-	u.pnum(1001)
-
-	//挂机开始
-	timer.AfterFunc(10*time.Second, 0, func(n int) {
-		u.pnum(1)
-		u.sync()
-	})
-
-	timer.AfterFunc(30*time.Second, 0, func(n int) {
-		//u.chat(fmt.Sprintf("%d", gofunc.TimeNowUnix()))
-	})
-
-	time.Sleep(5 * time.Second)
-
-	timer.AfterFunc(10*time.Second, 0, func(n int) {
-		u.pnum(1001)
-	})
-
 }
 
 type user struct {
@@ -162,6 +204,7 @@ type user struct {
 }
 
 func newUser(uid uint64, conn *websocket.Conn) *user {
+
 	u := new(user)
 	u.uid = uid
 	u.conn = conn
@@ -169,143 +212,103 @@ func newUser(uid uint64, conn *websocket.Conn) *user {
 
 	go u.readLoop()
 	go u.sendLoop()
-	//go u.ping()
 
 	return u
 }
 
 func (u *user) send(b []byte) {
+
 	u.sendChan <- b
 }
 
-func (u *user) sync() {
-	req := new(pb.SyncRequest)
-	req.SceneMon = proto.Int32(1)
-	b, _ := proto.Marshal(req)
-	u.send(pack(1005, b))
-}
-
-func (u *user) chat(msg string) {
-	req := new(pb.ChatSendMsgRequest)
-	req.Id = proto.Int32(1)
-	req.Msg = proto.String(msg)
-	b, _ := proto.Marshal(req)
-	u.send(pack(1701, b))
-}
-
-func (u *user) create() {
-	k := gofunc.RandInt(1, 9999)
-	name := fmt.Sprintf("%d%d", gofunc.TimeNowUnix(), k)
-	req := new(pb.UserCreateRequest)
-	req.Name = proto.String(name)
-	req.Avatar = proto.String("boy#aboy1_head_icon_png#aboy1_head,aboy1_body,boy1_foot")
-	req.Code = proto.String("1234")
-
-	b, _ := proto.Marshal(req)
-	u.send(pack(1011, b))
-}
-
 func (u *user) pnum(pnum uint16) {
+
 	u.send(pack(pnum, nil))
 }
 
-func (u *user) ping() {
-	for {
-		u.send(pack(1, nil))
-		time.Sleep(10 * time.Second)
-	}
-}
-
-func (u *user) readLoop() {
-	for {
-		_, resp, err := u.conn.ReadMessage()
-		if err != nil {
-			log.Println("read", u.uid, err)
-			return
-		}
-		pnum, code, body := unpack(resp)
-		if code != 0 {
-			log.Printf("uid=%d pnum=%d code=%d", u.uid, pnum, code)
-		} else {
-			u.printInfo(pnum, body)
-		}
-	}
-}
-
 func (u *user) sendLoop() {
+
 	for {
+
 		select {
+
 		case msg := <-u.sendChan:
+
 			err := u.conn.WriteMessage(websocket.BinaryMessage, msg)
 			if err != nil {
-				log.Printf("send wsconn uid=%d err=%v", u.uid, err)
+
+				logrus.Printf("send wsconn uid=%d err=%v", u.uid, err)
+
 				return
 			}
 		}
 	}
 }
 
-func (u *user) printInfo(pnum uint16, body []byte) {
-	switch pnum {
-	// case 900:
-	// 	resp := &pb.NotifyChatMsg{}
-	// 	err := proto.Unmarshal(body, resp)
-	// 	if err != nil {
-	// 		log.Println(err)
-	// 		return
-	// 	}
-	// 	log.Println("chat", resp)
-	case 1002:
-		userInfo := &pb.UserInfoResponse{}
-		err := proto.Unmarshal(body, userInfo)
+func (u *user) readLoop() {
+
+	for {
+
+		_, resp, err := u.conn.ReadMessage()
 		if err != nil {
-			log.Println(err)
+
+			logrus.Println("read", u.uid, err)
+
 			return
 		}
-		if userInfo.GetState() == 0 {
-			u.create()
+
+		pnum, code, body := unpack(resp)
+		if code != 0 {
+
+			logrus.Printf("uid=%d pnum=%d code=%d", u.uid, pnum, code)
+		} else {
+
+			u.printInfo(pnum, body)
 		}
-	case 1012:
-		resp := &pb.UserCreateResponse{}
-		err := proto.Unmarshal(body, resp)
-		if err != nil {
-			log.Println(err)
-			return
-		}
-		log.Println("create ok", resp.Data.GetUid())
 	}
 }
 
 func request(conn *websocket.Conn, reqpnum uint16, b []byte) (pnum uint16, code uint16, body []byte, err error) {
+
 	err = conn.WriteMessage(websocket.BinaryMessage, pack(reqpnum, b))
 	if err != nil {
-		log.Println("write", err)
+
+		logrus.Println("write", err)
+
 		return
 	}
 
-	log.Printf("request, pnum=%d", reqpnum)
+	logrus.Debugf("request, pnum=%d", reqpnum)
 
 	var resp []byte
 	_, resp, err = conn.ReadMessage()
 	if err != nil {
-		log.Println("read", err)
+
+		logrus.Println("read", err)
+
 		return
 	}
+
 	pnum, code, body = unpack(resp)
-	log.Printf("response, pnum=%d code=%d", pnum, code)
+	logrus.Debugf("response, pnum=%d code=%d", pnum, code)
+
 	return
 }
 
 func pack(pnum uint16, b []byte) []byte {
+
 	data := make([]byte, len(b)+2)
 	binary.BigEndian.PutUint16(data[0:2], pnum)
 	copy(data[2:], b)
+
 	return data
 }
 
 func unpack(b []byte) (pnum uint16, code uint16, body []byte) {
+
 	pnum = binary.BigEndian.Uint16(b[0:2])
 	code = binary.BigEndian.Uint16(b[2:4])
 	body = b[4:]
+
 	return
 }
--- test/run.go
+++ test/run.go
@@ -1,5 +1,38 @@
 package test
 
+import (
+	"os"
+	"os/signal"
+	"syscall"
+
+	"enen/test/robot"
+
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+)
+
+var quit = make(chan os.Signal, 1)
+
 func Run() {
 
+	if viper.GetBool("test.debug") {
+
+		logrus.SetLevel(logrus.DebugLevel)
+	} else {
+
+		logrus.SetLevel(logrus.InfoLevel)
+	}
+
+	switch viper.GetString("test.func") {
+
+	case "robot":
+
+		robot.Test()
+
+	default:
+
+	}
+
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
 }
